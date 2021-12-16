f9a1b4d5845b9ca7aa6f9c2d7f5331c83862f25f
ahmet <ahmet.altun60@gmail.com>
2021-12-16 23:56:49 +0300

    ön yüz düzenlemeleri yapıldı. carousel, spinner, breadcrumb eklendi

--- coregularcommerce.clientui/src/app/shop/product-details/product-details.component.ts
+++ coregularcommerce.clientui/src/app/shop/product-details/product-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { IProduct } from 'src/app/shared/modules/product';
+import { BreadcrumbService } from 'xng-breadcrumb';
 import { ShopService } from '../shop.service';
 
 @Component({
@@ -11,7 +12,9 @@ import { ShopService } from '../shop.service';
 export class ProductDetailsComponent implements OnInit {
   product: IProduct = {} as IProduct;
 
-  constructor(private shopService: ShopService, private activeRoute: ActivatedRoute) { }
+  constructor(private shopService: ShopService, private activeRoute: ActivatedRoute, private bcService: BreadcrumbService) {
+    this.bcService.set('@productDetails', ' ');
+  }
 
   ngOnInit(): void {
     this.getProductDetail();
@@ -21,6 +24,7 @@ export class ProductDetailsComponent implements OnInit {
     const id: number = Number(this.activeRoute.snapshot.paramMap.get('id'));
     this.shopService.getProduct(id).subscribe((product: IProduct) => {
       this.product = product;
+      this.bcService.set('@productDetails', product.name);
     }, (error) => {
       console.warn("Ürün detayı hatası: ", error);
     });
